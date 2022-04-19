caa34513ea4409b6ac30159b20c76c248582c5ed
Krultu <76265885+Krultu@users.noreply.github.com>
2022-04-18 23:23:15 -0300

    first commit

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
-try:
-    import requests, ctypes, time, os, threading, platform
-    from colorama import Fore
-except ImportError:
-    input("Error while importing modules. Please install the modules in requirements.txt")
+import tweepy
+import requests
+import ctypes
+import time 
+import os
+import threading
+import platform
+from colorama import Fore
+
+import random
+import string
 
 ascii_text = """
-        _   _ _    _        _    
-       | | (_) |  | |      | |   
-       | |_ _| | _| |_ ___ | | __
-       | __| | |/ / __/ _ \| |/ /
-       | |_| |   <| || (_) |   < 
-        \__|_|_|\_\\__\___/|_|\ _\\
+  ____             _       _   __  __          _ _         _   _                  ____ _               _             
+ / ___|  ___   ___(_) __ _| | |  \/  | ___  __| (_) __ _  | | | |___  ___ _ __   / ___| |__   ___  ___| | _____ _ __ 
+ \___ \ / _ \ / __| |/ _` | | | |\/| |/ _ \/ _` | |/ _` | | | | / __|/ _ \ '__| | |   | '_ \ / _ \/ __| |/ / _ \ '__|
+  ___) | (_) | (__| | (_| | | | |  | |  __/ (_| | | (_| | | |_| \__ \  __/ |    | |___| | | |  __/ (__|   <  __/ |   
+ |____/ \___/ \___|_|\__,_|_| |_|  |_|\___|\__,_|_|\__,_|  \___/|___/\___|_|     \____|_| |_|\___|\___|_|\_\___|_|                                                                                                                                                                                     
   """
-
 
 if platform.system() == "Windows":
     clear = "cls"
 else:
     clear = "clear"
 
-class tiktok:
+website = "https://www.site.com/"
+
+class userchecker:
 
     def __init__(self):
         self.lock = threading.Lock()
@@ -32,7 +38,7 @@ class tiktok:
     def update_title(self):
         remaining = len(self.usernames) - (self.available + self.unavailable)
         ctypes.windll.kernel32.SetConsoleTitleW(
-            f"TikTok Username Checker | Available: {self.available} | Unavailable: {self.unavailable} | Checked: {(self.available + self.unavailable)} | Remaining: {remaining} | Developed by @useragents on Github"
+            f"Social Media User Checker | Available: {self.available} | Unavailable: {self.unavailable} | Checked: {(self.available + self.unavailable)} | Remaining: {remaining} | @Krultu"
         )
     
     def safe_print(self, arg):
@@ -40,11 +46,11 @@ class tiktok:
         print(arg)
         self.lock.release()
     
-    def print_console(self, status, arg, color = Fore.RED):
+    def print_console(self, status, arg, color = Fore.LIGHTMAGENTA_EX):
         self.safe_print(f"       {Fore.WHITE}[{color}{status}{Fore.WHITE}] {arg}")
     
-    def check_username(self, username):
-        if username.is_digit():
+    def check_username(self, username, ws):
+        if username.isdigit():
             self.unavailable += 1
             self.print_console("Unavailable", username)
             return
@@ -55,13 +61,14 @@ class tiktok:
                 "accept-language": "en-US",
                 "content-type": "application/json"
             }
-            r = session.head("https://www.tiktok.com/@{}".format(username), headers = headers)
+            r = session.head(ws + username + "/", headers=headers)
+
             if r.status_code == 200:
                 self.unavailable += 1
-                self.print_console("Unavailable", username)
+                self.print_console("Unavailable", username, Fore.RED)
             elif r.status_code == 404:
                 self.available += 1
-                self.print_console("Available", username, Fore.GREEN)
+                self.print_console("MAYBE Available", username, Fore.GREEN)
                 with open("Available.txt", "a") as f:
                         f.write(username + "\n")
             self.update_title()
@@ -83,17 +90,51 @@ class tiktok:
     
     def main(self):
         os.system(clear)
+
+        try:
+            os.remove("Available.txt")
+        except:
+            print()
+
+        try:
+            os.remove("usernames.txt")
+        except:
+            print()
+
         if clear == "cls":
-            ctypes.windll.kernel32.SetConsoleTitleW("TikTok Username Checker | Developed by @useragents on Github")
-        print(Fore.RED + ascii_text)
+            ctypes.windll.kernel32.SetConsoleTitleW("Social Media User Checker | @Krultu")
+        print(Fore.LIGHTMAGENTA_EX + ascii_text)
+        print(Fore.LIGHTMAGENTA_EX + "// Twitter: @kruitu | Discord: Krultu#6749")
+
+        output_file = "usernames.txt"
+        smoption = int(input("\n[CONSOLE] Choose a social media \n    1 - Tiktok \nOption: "))
+        if smoption == 1:
+            website = "https://www.tiktok.com/@"
+        # elif smoption == 2:
+        #     website = "https://twitter.com/"
+        else:
+            os._exit(0)
+        amount = int(input("\n[CONSOLE] Amount of usernames: "))
+        character_amount = int(input("\n[CONSOLE] Amount of characters per username: "))
+        letteronly = int(input("\n[CONSOLE] Letter only [0,1]: "))
+
+
+        for i in range(amount):
+            if letteronly == 1:
+                generated = ("").join(random.choices(string.ascii_letters, k = character_amount))
+                with open(output_file, "a") as f:
+                    f.write(generated + "\n")
+            else:
+                generated = ("").join(random.choices(string.ascii_letters + string.digits, k = character_amount))
+                with open(output_file, "a") as f:
+                    f.write(generated + "\n")
+        
         self.load_usernames()
-        threads = int(input(f"       {Fore.WHITE}[{Fore.RED}Console{Fore.WHITE}] Threads: "))
-        print()
-        if threads >= 5: #To prevent ratelimits
-            threads = 5
+        threads = 5
+        
         
         def thread_starter():
-            self.check_username(self.usernames[self.counter])
+            self.check_username(self.usernames[self.counter], website)
         while self.checking:
             if threading.active_count() <= threads:
                 try:
@@ -104,6 +145,17 @@ class tiktok:
                 if len(self.usernames) <= self.counter:
                     self.checking = None
 
-obj = tiktok()
+obj = userchecker()
 obj.main()
-input()
+
+try:
+    os.remove("usernames.txt")
+except:
+    print()
+
+
+try:
+    os.startfile("Available.txt")
+except:
+    input()
+       
